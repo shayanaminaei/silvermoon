1e409bf3db74a48af5aa4d7eaa492e63c6fc2f6e
Shayan Aminaei <shayanaminaei@gmail.com>
2025-05-30 19:56:39 +0330

    product+ / superuseradmin +

--- SilverMoon/productions/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'productions'
--- SilverMoon/productions/context_proccessor.py
+++
@@ -1,5 +0,0 @@
-from .models import Category
-
-
-def categories(request):
-    return {'categories_context': Category.objects.all(), }
